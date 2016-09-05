80cf8859642b03d1cc0fdd2c32246adb196017c9
solenehuault <solene.huault@gmail.com>
2016-09-05 11:49:58 +0200

    release before 2000

--- list_of_movies/index.php
+++ list_of_movies/index.php
@@ -5,15 +5,19 @@
 		<title>Cooking Data 2/2</title>
 	</head>
 	<body>
+		<h1>List of Movies</h1>
 		<?php
 			$string = file_get_contents("films.json", FILE_USE_INCLUDE_PATH);
 			$brut = json_decode($string, true);
 			$top = $brut["feed"]["entry"];
 
+			$before_2000 = 0;
 			echo "<ol>";
 			for ($i = 0 ; $i < count($top) ; $i++) {
 				$movie = $top[$i];
 				$title = $movie['im:name'];
+				$release = $movie['im:releaseDate'];
+				$year = explode("-", $release[label]);
 				if ($i < 10) {
 					echo "<li>".$title[label]."</li>";
 				}
@@ -25,12 +29,16 @@
 					$director_lego = $top[$rank_lego][title][label];
 					$dir = explode("-", $director_lego);
 				}
+				if ($year[0] < 2000) {
+					$before_2000++;
+				}
 			}
 			echo "</ol>";
 			echo "<p>Gravity is ranked ".$rank_gravity.".</p>";
 			print_r($top[$rank_lego]);
 			print_r($dir);
 			echo "<p>The directors of ".$dir[0]." are ".$dir[count($dir)-1].".</p>";
+			echo "<p>".$before_2000." movies were released before 2000";
 		?>
 	</body>
 </html>
